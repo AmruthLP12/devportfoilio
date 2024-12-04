eae22fcd1e0af2cbe4e76454be766f1da3102962
Amruth L P <amruthlp12@gmail.com>
2024-12-04 14:06:23 +0530

    custom-scrollbar added

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="custom-scrollbar">
       <body className={font.className}>
         <div className="hidden md:block">
           <AnimatedCursor innerSize={8} outerSize={35} innerScale={2} outerScale={2} outerAlpha={0} innerStyle={{
